88134e1f1e94ce2b2309cc82019b1cd17e709987
Joel Kizito <kizitojoel16@gmail.com>
2023-03-16 15:12:23 -0700

    Complete task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
-int main()
+/**
+ * 
+ * main - This is the main function of the program
+*/
+int main(void)
 {
 	char my_str[] = "with proper grammar, but the outcome is a piece of art,";
 	printf("%s\n", my_str);
 	return(0);
 }
+
