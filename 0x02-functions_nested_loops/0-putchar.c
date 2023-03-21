43d3daeabd92ecad6dad137412d737e92836906a
Joel Kizito <kizitojoel16@gmail.com>
2023-03-21 07:35:17 -0700

    Complete task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,18 +3,18 @@
 
 /**
  * main - main function
- * 
+ *
  * Description: prints _putchar
  * Return: Returns 0
  */
 int main(void)
 {
 	int i;
-
 	char my_string[] = "_putchar";
+
 	for (i = 0; i < (int) strlen(my_string); i++)
 		_putchar(my_string[i]);
 	_putchar('\n');
-	return 0;
+	return (0);
 }
 
