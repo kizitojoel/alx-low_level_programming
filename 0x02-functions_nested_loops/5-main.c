2a8f995ad01d34dbd4c70610d1a2512082d7a97a
Joel Kizito <kizitojoel16@gmail.com>
2023-03-21 12:40:46 -0700

    Complete task 4 and 5

---
+++ 0x02-functions_nested_loops/5-main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - mainloop
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int r;
+
+	r = print_sign(98);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	r = print_sign(-1);	
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - prints sign
+ * @n - the test number
+ * Description: Prints the sign of number
+ * Return: returns 1 if greater than 0, 0 if it is zero, and -1 if less than 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
+
