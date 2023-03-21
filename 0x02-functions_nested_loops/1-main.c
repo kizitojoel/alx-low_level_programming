ed8115e0274cd5962a01f0d4b600882d3c5f04c5
Joel Kizito <kizitojoel16@gmail.com>
2023-03-21 07:49:30 -0700

    Complete task 1

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - main function
+ *
+ * Description: Prints the lowercase alphabet
+ * Return: Returns 0 when successful
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
