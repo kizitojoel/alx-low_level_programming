67bc8644e6ba1a55dae29c822bf6d98fcde66d10
Joel Kizito <kizitojoel16@gmail.com>
2023-03-21 12:03:50 -0700

    Complete task 3

---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ *
+ * Return: Always zero
+ */
+int main(void)
+{
+	int r;
+	r = _islower('H');
+	_putchar(r + '0');
+	r = _islower('o');
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
+}
