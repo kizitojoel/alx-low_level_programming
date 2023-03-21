8bc52b81b5f53b023726addab26e274a3b5746da
Joel Kizito <kizitojoel16@gmail.com>
2023-03-21 07:53:08 -0700

    Complete task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,17 @@
 #include "main.h"
 
+/**
+ * print_alphabet - print lowercase alphabet
+ *
+ * Description: print lowercase alphabet
+ * Return: nothing
+ */
 void print_alphabet(void)
 {
-        int i;
+	int i;
 
-        for (i = 97; i <= 122; i++)
-                _putchar(i);
-        _putchar('\n');
+	for (i = 97; i <= 122; i++)
+		_putchar(i);
+	_putchar('\n');
 }
+
