7d54d351a688ceb052c3509a0fa4f1737d95e49c
Joel Kizito <kizitojoel16@gmail.com>
2023-03-22 03:12:37 -0700

    Complete task 9

---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - main loop
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	times_table();
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * times_table - prints the times table up to 10
+ *
+ * Description: Uses two nested for loops
+ * Return: Nothing
+ */
+void times_table(void)
+{
+	int i, j, product;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			product = i * j;
+			if (j > 0)
+			{	
+				_putchar(44);
+				if (product >= 10)
+				{
+					_putchar(32);
+					_putchar((product / 10) + '0');	
+				}
+				else if (product < 10)
+				{
+					_putchar(32);
+					_putchar(32);
+				}
+			}
+			_putchar((product % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
+
