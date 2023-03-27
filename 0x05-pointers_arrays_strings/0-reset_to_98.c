d42af725dd0635a4ce851db68db3718e7ae624ea
Joel Kizito <kizitojoel16@gmail.com>
2023-03-27 05:38:14 -0700

    Complete task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - returns a number to 98
+ * @n: takes in the address to number
+ * Description: uses deferening
+ * Return: Nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+# Assignments for pointers, arrays and strings
