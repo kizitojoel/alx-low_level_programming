b4c73f25f645b96ae8aa5ead1788bdfb543780dc
Joel Kizito <kizitojoel16@gmail.com>
2023-03-30 02:30:04 -0700

    Complete task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,6 @@
  * _strcmp - compares two strings
  * @s1: base string
  * @s2: string to be compared
- * 
  * Return: negative if it is less than, zero if equal, positive if greater than
  */
 int _strcmp(char *s1, char *s2)
@@ -16,8 +15,7 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
-		else
-			i++;
+		i++;
 	}
 	return (s1[i] - s2[i]);
 }
