24a85b08049808d678403daee4f79e1a40550c69
Joel Kizito <kizitojoel16@gmail.com>
2023-04-05 03:42:15 -0700

    Complete task 1 and 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - prints the length of a string
+ * @s: the string pointer
+ *
+ * Return: the lenght of a string(int)
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
