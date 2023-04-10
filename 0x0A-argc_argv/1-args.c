91f276c58bcd1f8449962fad2305c74c3c2363ab
Joel Kizito <kizitojoel16@gmail.com>
2023-04-10 13:15:19 -0700

    Complete task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - checks main function
+ * @argc: the number
+ * @argc: argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, __attribute__ ((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	exit(EXIT_SUCCESS);
+}
