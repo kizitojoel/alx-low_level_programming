4f0138824eae3830453f9cc42cb6c904c717f1b6
Joel Kizito <kizitojoel16@gmail.com>
2023-04-11 13:52:46 -0700

    Complete strcat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * str_concat - returns the pointer to concatenated strings
+ * @s1: the first string
+ * @s2: the second string
+ *
+ * Return: the pointer to the concatenated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int count1 = 0;
+	int count2 = 0;
+	int i = 0;
+	char *concat;
+	char *copy;
+
+	copy = s1;
+	while (*copy != '\0')
+	{
+		copy++;
+		count1++;
+	}
+	copy = s2;
+	while (*copy != '\0')
+	{
+		copy++;
+		count2++;
+	}
+	concat = malloc(sizeof(char) * (count1 + count2 + 1));
+	if (concat == NULL)
+		return (NULL);
+	for (i = 0; i <= count1; i++)
+	{
+		concat[i] = s1[i];
+	}
+	for (i = count1; i <= count1 + count2; i++)
+	{
+		concat[i] = s2[i - count1];
+	}
+	concat[count1 + count2 + 1] = '\0';
+	return (concat);
+}
