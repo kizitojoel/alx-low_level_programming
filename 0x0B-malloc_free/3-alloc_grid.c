621578e73cba0d60ea5fe66d975a555629890930
Joel Kizito <kizitojoel16@gmail.com>
2023-04-11 15:07:39 -0700

    Allocate grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,7 +24,7 @@ int **alloc_grid(int width, int height)
 		grid[i] = malloc(sizeof(**grid) * width);
 		if (grid[i] == NULL)
 		{
-			for (i--; i > 0; i--)
+			for (i--; i >= 0; i--)
 			{
 				free(grid[i]);
 			}
