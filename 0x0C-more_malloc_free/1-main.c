4cc878b426449ec2dafcb5a2746ada0679b97533
Joel Kizito <kizitojoel16@gmail.com>
2023-04-13 13:20:59 -0700

    Complete task 1

--- 0x0C-more_malloc_free/1-main.c
+++ 0x0C-more_malloc_free/1-main.c
@@ -10,9 +10,15 @@
 int main(void)
 {
 	char *concat;
+	char *concat1;
+	char *concat2;
 
 	concat = string_nconcat("Best ", "School !!!", 6);
+	concat1 = string_nconcat(NULL, "HELLO", 3);
+	concat2 = string_nconcat("There", "fore", 5);
 	printf("%s\n", concat);
+	printf("%s\n", concat1);
+	printf("%s\n", concat2);
 	free(concat);
 	return (0);
 }
--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,15 +17,23 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int count1 = 0;
 	unsigned int count2 = 0;
 
-	ptr1 = s1;
-	while (*ptr1 != '\0')
-	{	
-		count1++;
-		ptr1++;
+	if (s1 == NULL)
+		s1 = "";
+	else
+	{
+		ptr1 = s1;
+		while (*(ptr1++) != '\0')
+			count1++;
+	}
+	if (s2 == NULL)
+		s2 = "";
+	else
+	{
+		ptr1 = s2;
+		while (*(ptr1++) != '\0')
+			count2++;
 	}
 	ptr1 = s2;
-	while (*(ptr1++) != '\0')
-		count2++;
 	str = malloc(sizeof(char) * (count1 + n + 1));
 	if (str == NULL)
 		exit(98);
@@ -33,8 +41,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; i < count1; i++)
 		str[i] = s1[i];
 	ptr1 = s2;
-	for (i = count1; i < (count1 + n) && s2[i - count1]; i++)
+	for (i = count1; i < (count1 + n); i++)
 		str[i] = s2[i - count1];
-	str[count1 + n + 1] = '\0';
+	str[count1 + n] = '\0';
 	return (str);
 }
