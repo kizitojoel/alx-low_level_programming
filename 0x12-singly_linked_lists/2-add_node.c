ad00585a83911904a110b7b621ad981e3b395cde
Joel Kizito <kizitojoel16@gmail.com>
2023-04-27 08:13:22 -0700

    Complete add new node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -6,7 +6,7 @@
 /**
  * add_node - adds a node to the beginning
  * @head: the pointer to the pointer to the head
- * str: the string that needs to be copied to the node
+ * @str: the string that needs to be copied to the node
  * Return: the address of the new element or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
@@ -28,13 +28,13 @@ list_t *add_node(list_t **head, const char *str)
 /**
  * _strlen - counts the length of character of a string
  * @s: the string pointer
- * 
  * Return: the number of characters in a string
  */
 unsigned int _strlen(char *s)
 {
 	unsigned int count = 0;
-	while(*s != '\0')
+
+	while (*s != '\0')
 	{
 		count++;
 		s++;
