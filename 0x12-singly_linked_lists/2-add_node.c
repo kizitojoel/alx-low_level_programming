b652fa558401ca5c40bbfbb1778cd3508ef7d2ba
Joel Kizito <kizitojoel16@gmail.com>
2023-04-27 08:10:13 -0700

    Complete add new node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,43 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_node - adds a node to the beginning
+ * @head: the pointer to the pointer to the head
+ * str: the string that needs to be copied to the node
+ * Return: the address of the new element or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	char *duplicate;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	duplicate = strdup(str);
+	new->str = duplicate;
+	new->len = _strlen(duplicate);
+	new->next = *head;
+	*head = new;
+	return (new);
+}
+
+/**
+ * _strlen - counts the length of character of a string
+ * @s: the string pointer
+ * 
+ * Return: the number of characters in a string
+ */
+unsigned int _strlen(char *s)
+{
+	unsigned int count = 0;
+	while(*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	return (count);
+}
