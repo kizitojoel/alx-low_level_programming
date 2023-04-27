b3be26591822fff3b93dd90e39bc55df0dbac3bd
Joel Kizito <kizitojoel16@gmail.com>
2023-04-27 11:01:00 -0700

    Implement constructor attribute

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include "lists.h"
 
-void print_before()
+/**
+ * print_before - prints before main function
+ *
+ * Return: nothing
+ */
+void print_before(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -20,5 +20,5 @@ list_t *add_node(list_t **head, const char *str);
 unsigned int _strlen(const char *s);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-void print_before() __attribute__((constructor));
+void print_before(void) __attribute__((constructor));
 #endif
