10019ad8f2a1448a8828288e6e30f7bcc9b30a49
Joel Kizito <kizitojoel16@gmail.com>
2023-05-07 06:39:53 -0700

    Convert binary to unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * binary_to_uint - converts a binary to decimal
+ * @b: the pointer to the string
+ *
+ * Return: the unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int decimal = 0;
+	int count = 0;
+	const char *dup;
+
+	if (b == NULL)
+		return (0);
+	else
+		dup = b;
+	while (*dup != '\0')
+	{
+		if (*dup != '0' && *dup != '1')
+			return (0);
+		count++;
+		dup++;
+	}
+	count -= 1;
+	while (*b != '\0')
+	{
+		if (*b == '1')
+			decimal += power(count);
+		count--;
+		b++;
+	}
+	return (decimal);
+}
+/**
+ * power - return two to the power of a specified int
+ * @n: the power to which we are returning
+ *
+ * Return: the (int) value of 2 power n
+ */
+int power(int n)
+{
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		return (0);
+	return (2 * power(n - 1));
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,5 @@
+#ifndef _BIT_HEADER_
+#define _BIT_HEADER_
+unsigned int binary_to_uint(const char *b);
+int power(int n);
+#endif
