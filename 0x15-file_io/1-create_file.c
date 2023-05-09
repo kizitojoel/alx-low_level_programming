32288335ae89986a61a2c5e21e80da5c3267cd4d
Joel Kizito <kizitojoel16@gmail.com>
2023-05-09 07:41:25 -0700

    Fix task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_RDWR | O_APPEND | O_CREAT, 0600);
+	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
@@ -34,6 +34,6 @@ int create_file(const char *filename, char *text_content)
 	wr_status = write(fd, text_content, count);
 	if (wr_status == -1)
 		return (-1);
-	close (fd);
+	close(fd);
 	return (1);
 }
