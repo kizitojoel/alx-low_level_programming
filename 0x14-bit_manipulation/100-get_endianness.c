efa92da31d57432e8f1f71627facb2f2da5c7a78
Joel Kizito <kizitojoel16@gmail.com>
2023-05-22 06:12:16 -0700

    Test endiannes

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endiannes of a system
+ *
+ * Return: 0 if big endian 1 if little
+ */
+int get_endianness(void)
+{
+	int test = 2;
+
+	if (c & 1)
+		return (0);
+	else
+		return (1);
+}
